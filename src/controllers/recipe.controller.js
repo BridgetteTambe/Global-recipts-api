a342a49087508229eece6e0897f22ca2937a3841
BridgetteTambe <bridgettetambe@gmail.com>
2026-05-18 13:12:51 +0100

    Added soome functionalities in controllers

--- src/controllers/recipe.controller.js
+++ src/controllers/recipe.controller.js
@@ -13,8 +13,6 @@ const createRecipe = async (req, res, next) => {
   }
 };
 
-
-
 const recipeService = require("../services/recipe.service");
 const getAllRecipes = async (req, res, next) => {
   try {
@@ -30,3 +28,72 @@ const getAllRecipes = async (req, res, next) => {
     next(error); 
   }
 };
+
+
+const getRecipeById = async (req, res, next) => {
+  try {
+    const recipe = await recipeService.getRecipeById(req.params.id);
+
+    if (!recipe) {
+      // Create an error object and pass it to the global error handler
+      const error = new Error(`Recipe with ID ${req.params.id} is not found try again`);
+      error.statusCode = 404;
+      return next(error);
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: recipe,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+// Updating receipt by id
+const updateRecipe = async (req, res, next) => {
+  try {
+    const updatedRecipe = await recipeService.updateRecipe(
+      req.params.id,
+      req.body
+    );
+
+    if (!updatedRecipe) {
+      const error = new Error(`Recipe with ID ${req.params.id} is not found try again`);
+      error.statusCode = 404;
+      return next(error);
+    }
+
+    res.status(200).json({
+      status: "success",
+      message: "Recipe updated successfully",
+      data: updatedRecipe,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// DELETE /recipes/:id
+const deleteRecipe = async (req, res, next) => {
+  try {
+    const deletedRecipe = await recipeService.deleteRecipe(req.params.id);
+
+    if (!deletedRecipe) {
+      const error = new Error(`Recipe with ID ${req.params.id} is not found try again`);
+      error.statusCode = 404;
+      return next(error);
+    }
+
+    res.status(200).json({
+      status: "success",
+      message: "Recipe deleted successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+
